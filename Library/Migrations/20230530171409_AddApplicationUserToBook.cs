db59316039e41f5f09714700aaeaa34b1bd6cfe3
Sarah Reimann <reimannsarah45@gmail.com>
2023-05-30 10:14:41 -0700

    AddApplicationUserToBook
    co-authored-by: Emma Gerigscott <elgerig@gmail.com>

---
+++ Library/Migrations/20230530171409_AddApplicationUserToBook.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Library.Migrations
+{
+    public partial class AddApplicationUserToBook : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "UserId",
+                table: "Books",
+                type: "varchar(255)",
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Books_UserId",
+                table: "Books",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Books_AspNetUsers_UserId",
+                table: "Books",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Books_AspNetUsers_UserId",
+                table: "Books");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Books_UserId",
+                table: "Books");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Books");
+        }
+    }
+}
