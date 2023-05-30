6d09bb7e8a4f94606903f961d6d21f42bc16834c
Sarah Reimann <reimannsarah45@gmail.com>
2023-05-30 10:06:03 -0700

    AddAuthorEntity
    co-authored-by: Emma Gerigscott <elgerig@gmail.com>

--- Library/Migrations/20230530170514_AddAuthorEntity.Designer.cs
+++ Library/Migrations/20230530170514_AddAuthorEntity.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Library.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Library.Migrations
 {
     [DbContext(typeof(LibraryContext))]
-    partial class LibraryContextModelSnapshot : ModelSnapshot
+    [Migration("20230530170514_AddAuthorEntity")]
+    partial class AddAuthorEntity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
