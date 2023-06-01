a4c523f16d08e4d59b7c70e9eddd8af38d66a8f6
Sarah Reimann <reimannsarah45@gmail.com>
2023-06-01 11:45:51 -0700

    add author search
    co-authored-by: Emma Gerigscott <elgerig@gmail.com>

--- Library/Controllers/SearchController.cs
+++ Library/Controllers/SearchController.cs
@@ -33,10 +33,34 @@ namespace Library.Controllers
     [HttpPost]
     public ActionResult TitleSearch(string title)
     {
-      List<Book> foundBooks = _db.Books.Where(book => book.Title == title).ToList();
-                                // .Include(book => book.AuthorJoinEntities)
-                                // .ThenInclude(join => join.Author)
-      return RedirectToAction("Results",foundBooks);
+      List<Book> foundBooks = _db.Books
+                                .Include(book => book.AuthorJoinEntities)
+                                .ThenInclude(join => join.Author)
+                                .Where(book => book.Title.Contains(title)).ToList();
+      return View("Results",foundBooks);
+    }
+
+    public ActionResult AuthorSearch() => View();
+
+    [HttpPost]
+    public ActionResult AuthorSearch(string author)
+    {
+      List<Book> foundBooks = _db.Books
+                                .Include(book => book.AuthorJoinEntities)
+                                .ThenInclude(join => join.Author)
+                                .ToList();
+      List<Book> results = new List<Book>();
+      foreach (Book book in foundBooks)
+      {
+        foreach (AuthorBook join in book.AuthorJoinEntities)
+        {
+          if (join.Author.Name.Contains(author))
+          {
+            results.Add(book);
+          }
+        }
+      }
+      return View("Results", results);
     }
 
     public ActionResult Results(List<Book> results)
